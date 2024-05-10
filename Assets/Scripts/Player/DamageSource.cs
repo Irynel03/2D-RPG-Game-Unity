7ff5be7a72e687b9adedccd91f28b2eb05f3767a
Irinel Ene <andreimc49@gmail.com>
2024-05-10 14:51:43 +0300

    project uploaded
    a

---
+++ Assets/Scripts/Player/DamageSource.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSource : MonoBehaviour
+{
+    [SerializeField] private int damageAmount = 1;
+
+    private void Start() {
+        MonoBehaviour currentActiveWeapon = ActiveWeapon.Instance.CurrentActiveWeapon;
+        damageAmount = (currentActiveWeapon as IWeapon).GetWeaponInfo().weaponDamage;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+        if(enemyHealth!=null){         
+            enemyHealth.TakeDamage(damageAmount);
+        }
+    }
+
+}
